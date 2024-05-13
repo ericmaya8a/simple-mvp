7f24039894af8eaa780c19aa2f42be8905b2ef59
Eric Maya <ericmaya8a@gmail.com>
2024-05-13 14:36:41 -0600

    chore: update server action

--- src/_server/actions/index.ts
+++ src/_server/actions/index.ts
@@ -1,13 +1,18 @@
 "use server";
 
 import { prisma } from "@/config/db/client";
+import { PaymentType } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 
 const FormSchema = z.object({
   studentId: z.string(),
   amount: z.coerce.number(),
-  paymentType: z.enum(["CASH", "CARD", "TRANSFER"]),
+  paymentType: z.enum([
+    PaymentType.CARD,
+    PaymentType.CASH,
+    PaymentType.TRANSFER,
+  ]),
 });
 
 export async function addTuition(formData: FormData) {
