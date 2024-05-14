2a0c9c0a3ae7ba799b7ac44d9499dd1df213c61b
Eric Maya <ericmaya8a@gmail.com>
2024-05-14 11:46:50 -0600

    chore: refactor TuitionForm

--- src/components/TuitionForm.tsx
+++ src/components/TuitionForm.tsx
@@ -7,56 +7,60 @@ export function TuitionForm({ studentId }: { studentId: string }) {
   return (
     <form action={addTuition}>
       <input type="hidden" name="studentId" value={studentId} />
-      <div className="mb-3">
-        <label className="block" htmlFor="date">
-          Date
-        </label>
-        <input
-          className="border rounded-md p-2"
-          type="date"
-          name="date"
-          id="date"
-          defaultValue={new Date().toISOString().split("T")[0]}
-          required
-        />
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-4">
+        <div>
+          <label className="block" htmlFor="date">
+            Date
+          </label>
+          <input
+            className="border rounded-md p-2"
+            type="date"
+            name="date"
+            id="date"
+            defaultValue={new Date().toISOString().split("T")[0]}
+            required
+          />
+        </div>
+        <div>
+          <label className="block" htmlFor="amount">
+            Amount
+          </label>
+          <input
+            className="border rounded-md p-2"
+            type="number"
+            name="amount"
+            id="amount"
+            min="0"
+            step=".01"
+            required
+          />
+        </div>
+        <div>
+          <label className="block" htmlFor="paymentType">
+            Payment Type
+          </label>
+          <select
+            className="border rounded-md p-2"
+            name="paymentType"
+            id="paymentType"
+            required
+          >
+            {options.map((option) => (
+              <option value={option} key={option}>
+                {option.toLowerCase()}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex items-end">
+          <button
+            className="p-3 bg-blue-600 text-white uppercase rounded w-20"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <div className="mb-3">
-        <label className="block" htmlFor="amount">
-          Amount
-        </label>
-        <input
-          className="border rounded-md p-2"
-          type="number"
-          name="amount"
-          id="amount"
-          min="0"
-          step=".01"
-          required
-        />
-      </div>
-      <div className="mb-3">
-        <label className="block" htmlFor="paymentType">
-          Payment Type
-        </label>
-        <select
-          className="border rounded-md p-2"
-          name="paymentType"
-          id="paymentType"
-          required
-        >
-          {options.map((option) => (
-            <option value={option} key={option}>
-              {option.toLowerCase()}
-            </option>
-          ))}
-        </select>
-      </div>
-      <button
-        className="p-3 bg-blue-600 text-white uppercase rounded"
-        type="submit"
-      >
-        Add
-      </button>
     </form>
   );
 }
